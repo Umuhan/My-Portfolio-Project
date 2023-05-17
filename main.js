e890d708921f6427aca352e33e86eecfa265939a
Umu <hanny.ally123@gmail.com>
2023-05-17 14:00:59 +0300

    Fix js linters.

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  initApp();
-});
+const toggleButtonAndMenu = () => {
+  const menuBtn = document.querySelector('.burger-btn');
+  const list = document.querySelector('.nav-list');
+  menuBtn.classList.toggle('open');
+  list.classList.toggle('open');
+};
 
 const initApp = () => {
-  const menuBtn = document.querySelector(".burger-btn");
-  const list = document.querySelector(".nav-list");
-  menuBtn.addEventListener("click", toggleButtonAndMenu);
-  list.addEventListener("click", toggleButtonAndMenu);
+  const menuBtn = document.querySelector('.burger-btn');
+  const list = document.querySelector('.nav-list');
+  menuBtn.addEventListener('click', toggleButtonAndMenu);
+  list.addEventListener('click', toggleButtonAndMenu);
 };
 
-const toggleButtonAndMenu = () => {
-  const menuBtn = document.querySelector(".burger-btn");
-  const list = document.querySelector(".nav-list");
-  menuBtn.classList.toggle("open");
-  list.classList.toggle("open");
-};
+document.addEventListener('DOMContentLoaded', () => {
+  initApp();
+});
