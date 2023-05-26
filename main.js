930e4230400048d4e9f3d6681af4e18093b78a14
Umu <hanny.ally123@gmail.com>
2023-05-27 01:29:42 +0300

    Update preservation code.

--- main.js
+++ main.js
@@ -1,117 +1,117 @@
 const projects = [
   {
-    name: "Keeping track of hundreds of components",
+    name: 'Keeping track of hundreds of components',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.</br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,<br> when an unknown printer took a galley of type and scrambled it 1960s with the relea </br> Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s </br> Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s </br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
 
     image: {
-      mobile: "./images/Snapshoot Portfolio (1).png",
-      desktop: "./images/Container/Snapshoot Portfolio.png",
+      mobile: './images/Snapshoot Portfolio (1).png',
+      desktop: './images/Container/Snapshoot Portfolio.png',
     },
-    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
-    liveLink: "https://example.com/live1",
-    sourceLink: "https://example.com/source1",
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    liveLink: 'https://example.com/live1',
+    sourceLink: 'https://example.com/source1',
   },
 
   {
-    name: "Keeping track of hundreds of components",
+    name: 'Keeping track of hundreds of components',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.</br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</br> when an unknown printer took a galley of type and scrambled it 1960s with the relea </br> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s </br> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s </br> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
     image: {
-      mobile: "./images/Snapshoot Portfolio (1).png",
-      desktop: "./images/Container/Snapshoot Portfolio.png",
+      mobile: './images/Snapshoot Portfolio (1).png',
+      desktop: './images/Container/Snapshoot Portfolio.png',
     },
-    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
-    liveLink: "https://example.com/live2",
-    sourceLink: "https://example.com/source2",
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    liveLink: 'https://example.com/live2',
+    sourceLink: 'https://example.com/source2',
   },
 
   {
-    name: "Keeping track of hundreds of components",
+    name: 'Keeping track of hundreds of components',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.</br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</br> when an unknown printer took a galley of type and scrambled it 1960s with the relea </br> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s </br> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s </br> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
     image: {
-      mobile: "./images/Snapshoot Portfolio (1).png",
-      desktop: "./images/Container/Snapshoot Portfolio.png",
+      mobile: './images/Snapshoot Portfolio (1).png',
+      desktop: './images/Container/Snapshoot Portfolio.png',
     },
-    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
-    liveLink: "https://example.com/live2",
-    sourceLink: "https://example.com/source2",
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    liveLink: 'https://example.com/live2',
+    sourceLink: 'https://example.com/source2',
   },
 
   {
-    name: "Keeping track of hundreds of components",
+    name: 'Keeping track of hundreds of components',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.</br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</br>, when an unknown printer took a galley of type and scrambled it 1960s with the relea </br> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s </br> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s </br> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
     image: {
-      mobile: "./images/Snapshoot Portfolio (1).png",
-      desktop: "./images/Container/Snapshoot Portfolio.png",
+      mobile: './images/Snapshoot Portfolio (1).png',
+      desktop: './images/Container/Snapshoot Portfolio.png',
     },
-    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
-    liveLink: "https://example.com/live2",
-    sourceLink: "https://example.com/source2",
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    liveLink: 'https://example.com/live2',
+    sourceLink: 'https://example.com/source2',
   },
 
   {
-    name: "Keeping track of hundreds of components",
+    name: 'Keeping track of hundreds of components',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.</br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</br> when an unknown printer took a galley of type and scrambled it 1960s with the relea </br> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s </br> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s </br> Lorem Ipsum is simply dummy text of the printing and typesetting industry</br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
     image: {
-      mobile: "./images/Snapshoot Portfolio (1).png",
-      desktop: "./images/Container/Snapshoot Portfolio.png",
+      mobile: './images/Snapshoot Portfolio (1).png',
+      desktop: './images/Container/Snapshoot Portfolio.png',
     },
-    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
-    liveLink: "https://example.com/live2",
-    sourceLink: "https://example.com/source2",
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    liveLink: 'https://example.com/live2',
+    sourceLink: 'https://example.com/source2',
   },
 
   {
-    name: "Keeping track of hundreds of components",
+    name: 'Keeping track of hundreds of components',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.</br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</br> when an unknown printer took a galley of type and scrambled it 1960s with the relea </br> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s </br> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s </br> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
     image: {
-      mobile: "./images/Snapshoot Portfolio (1).png",
-      desktop: "./images/Container/Snapshoot Portfolio.png",
+      mobile: './images/Snapshoot Portfolio (1).png',
+      desktop: './images/Container/Snapshoot Portfolio.png',
     },
-    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
-    liveLink: "https://example.com/live2",
-    sourceLink: "https://example.com/source2",
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    liveLink: 'https://example.com/live2',
+    sourceLink: 'https://example.com/source2',
   },
 ];
 
-const projectsContainer = document.querySelector(".projects-container");
+const projectsContainer = document.querySelector('.projects-container');
 
 projects.forEach((project) => {
-  const projectElement = document.createElement("div");
-  projectElement.classList.add("project-box");
+  const projectElement = document.createElement('div');
+  projectElement.classList.add('project-box');
 
-  const imageElement = document.createElement("div");
-  imageElement.classList.add("project-img");
+  const imageElement = document.createElement('div');
+  imageElement.classList.add('project-img');
   projectElement.appendChild(imageElement);
 
-  const contentElement = document.createElement("div");
-  contentElement.classList.add("project-content");
+  const contentElement = document.createElement('div');
+  contentElement.classList.add('project-content');
   projectElement.appendChild(contentElement);
 
-  const titleElement = document.createElement("h3");
-  titleElement.classList.add("project-title");
+  const titleElement = document.createElement('h3');
+  titleElement.classList.add('project-title');
   titleElement.innerHTML = "Most-Post Storie's <br/> Gain+Glory";
   contentElement.appendChild(titleElement);
 
-  const langListElement = document.createElement("ul");
-  langListElement.classList.add("project-lang-used");
+  const langListElement = document.createElement('ul');
+  langListElement.classList.add('project-lang-used');
   contentElement.appendChild(langListElement);
 
   project.technologies.forEach((technology) => {
-    const langItemElement = document.createElement("li");
-    langItemElement.classList.add("lang");
+    const langItemElement = document.createElement('li');
+    langItemElement.classList.add('lang');
     langItemElement.textContent = technology;
     langListElement.appendChild(langItemElement);
   });
 
   // MODAL
   const displayPopup = (project) => {
-    const dynamicSection = document.getElementById("dynamic-section");
+    const dynamicSection = document.getElementById('dynamic-section');
     // const list = '';
     project.technologies.map((technology) => `<li>${technology}</li>`);
 
@@ -136,12 +136,12 @@ projects.forEach((project) => {
           <h2>${project.name}</h2>
           <div class='desktop-list'>
             <ul id='desktop-componets'>
-            ${project.technologies.map((ele) => `<li>${ele}</>`).join("")} 
+            ${project.technologies.map((ele) => `<li>${ele}</>`).join('')} 
             </ul>
           </div>
           <div class='mobile-list'>
             <ul id='mobile-componets'>
-            ${project.technologies.map((ele) => `<li>${ele}</>`).join("")}
+            ${project.technologies.map((ele) => `<li>${ele}</>`).join('')}
             </ul>
           </div>
           
@@ -178,83 +178,83 @@ projects.forEach((project) => {
   `;
     dynamicSection.innerHTML = sectioHTML;
 
-    const modelSection = document.querySelector(".model");
-    const modelCloseButton = document.querySelector(".model-close");
-    const overlay = document.getElementById("overlay");
+    const modelSection = document.querySelector('.model');
+    const modelCloseButton = document.querySelector('.model-close');
+    const overlay = document.getElementById('overlay');
 
-    modelCloseButton.addEventListener("click", () => {
-      modelSection.style.display = "none";
-      overlay.style.display = "none";
+    modelCloseButton.addEventListener('click', () => {
+      modelSection.style.display = 'none';
+      overlay.style.display = 'none';
     });
   };
 
   //  "See Project" button
-  const buttonElement = document.createElement("button");
-  buttonElement.classList.add("btn", "project-btn");
-  buttonElement.textContent = "See Project";
+  const buttonElement = document.createElement('button');
+  buttonElement.classList.add('btn', 'project-btn');
+  buttonElement.textContent = 'See Project';
   contentElement.appendChild(buttonElement);
 
   //  projects container
   projectsContainer.appendChild(projectElement);
-  buttonElement.addEventListener("click", () => {
+  buttonElement.addEventListener('click', () => {
     displayPopup(project);
 
-    const modelSection = document.querySelector(".model");
-    const overlay = document.getElementById("overlay");
-    modelSection.style.display = "block";
-    overlay.style.display = "block";
+    const modelSection = document.querySelector('.model');
+    const overlay = document.getElementById('overlay');
+    modelSection.style.display = 'block';
+    overlay.style.display = 'block';
   });
 });
 
 const toggleButtonAndMenu = () => {
-  const menuBtn = document.querySelector(".burger-btn");
-  const list = document.querySelector(".nav-list");
-  menuBtn.classList.toggle("open");
-  list.classList.toggle("open");
+  const menuBtn = document.querySelector('.burger-btn');
+  const list = document.querySelector('.nav-list');
+  menuBtn.classList.toggle('open');
+  list.classList.toggle('open');
 };
 
 const initApp = () => {
-  const menuBtn = document.querySelector(".burger-btn");
-  const list = document.querySelector(".nav-list");
-  menuBtn.addEventListener("click", toggleButtonAndMenu);
-  list.addEventListener("click", toggleButtonAndMenu);
+  const menuBtn = document.querySelector('.burger-btn');
+  const list = document.querySelector('.nav-list');
+  menuBtn.addEventListener('click', toggleButtonAndMenu);
+  list.addEventListener('click', toggleButtonAndMenu);
 };
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   initApp();
 });
 
 // form validation
-const form = document.querySelector(".form");
-const email = document.querySelector(".email");
-const emailError = document.querySelector(".error");
-const textMsg = "Please write the email in lowercase";
-const formName = document.querySelector(".user-name");
-const formmsg = document.querySelector(".msg");
+const form = document.querySelector('.form');
+const email = document.querySelector('.email');
+const emailError = document.querySelector('.error');
+const textMsg = 'Please write the email in lowercase';
+const formName = document.querySelector('.user-name');
+const formmsg = document.querySelector('.msg');
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   if (email.value !== email.value.toLowerCase()) {
     emailError.textContent = textMsg;
-    emailError.style.display = "flex";
-    emailError.style.color = "red";
-    emailError.style.fontSize = "16px";
-    emailError.style.marginTop = "5px";
+    emailError.style.display = 'flex';
+    emailError.style.color = 'red';
+    emailError.style.fontSize = '16px';
+    emailError.style.marginTop = '5px';
     e.preventDefault();
   } else {
-    emailError.style.display = "none";
-    email.style.border = "1px solid green";
+    emailError.style.display = 'none';
+    email.style.border = '1px solid green';
 
     const dataForm = {
       formName: formName.value,
       email: email.value,
       formmsg: formmsg.value,
     };
-    localStorage.setItem("dataForm", JSON.stringify(dataForm));
+    localStorage.setItem('dataForm', JSON.stringify(dataForm));
   }
 });
 
-window.addEventListener("DOMContentLoaded", () => {
-  const data = JSON.parse(localStorage.getItem("dataForm"));
+window.addEventListener('DOMContentLoaded', () => {
+  const data = JSON.parse(localStorage.getItem('dataForm'));
   if (data) {
     formName.value = data.formName;
     email.value = data.email;
